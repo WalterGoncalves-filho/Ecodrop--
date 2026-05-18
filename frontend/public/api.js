fada0d37fba82076b711745ea1c52dd5a0e4f10b
Walter Barauna <walterfilho@suprelogic.com>
2026-05-18 02:23:14 -0400

    Api do front apintando para o backend.

--- frontend/public/api.js
+++ frontend/public/api.js
@@ -1,4 +1,4 @@
-const API_BASE = `${window.location.protocol}//${window.location.hostname}:8000`;
+const API_BASE = `https://ecodrop-production.up.railway.app`;
 
 const api = {
   _getToken() { return localStorage.getItem('access_token'); },
